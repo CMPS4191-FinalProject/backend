037566964b0d7c828c9c5968513006a1b968b892
AlexDev404 <34188635+AlexDev404@users.noreply.github.com>
2025-11-12 14:59:57 -0600

    feat: create handlers

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -30,158 +30,398 @@ func (c *serverConfig) HealthCheckHandler(w http.ResponseWriter, r *http.Request
 	}
 }
 
-func (c *serverConfig) CreateQuoteHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var quote types.Quote
-	if err := c.readRequestJSON(w, r, &quote); err != nil {
+func (c *serverConfig) CreateUserHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var user types.User
+	if err := c.readRequestJSON(w, r, &user); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := database.ValidateQuote(quote); err != nil {
+	if err := database.ValidateUser(user); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := c.db.WriteQuote(quote); err != nil {
+	if err := c.db.CreateUser(user); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(quote)
+	json.NewEncoder(w).Encode(user)
 }
 
-func (c *serverConfig) GetQuotesHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (c *serverConfig) GetUsersHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Parse pagination and sorting parameters
 	limit, offset := parsePaginationParams(r)
 	sortBy, sortOrder := parseSortParams(r)
 
-	quotes, err := c.db.GetQuotesWithPagination(limit, offset, sortBy, sortOrder)
+	users, err := c.db.GetUsersWithPagination(limit, offset, sortBy, sortOrder)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	if len(quotes) == 0 {
-		quotes = []types.Quote{}
+	if len(users) == 0 {
+		users = []types.User{}
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(quotes)
+	json.NewEncoder(w).Encode(users)
 }
 
-func (c *serverConfig) UpdateQuoteHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// Extract the quote ID from the URL parameters
+func (c *serverConfig) GetUserHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the user ID from the URL parameters
 	idStr := ps.ByName("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid ID format", http.StatusBadRequest)
 		return
 	}
-	var updatedQuote types.Quote
-	if err := c.readRequestJSON(w, r, &updatedQuote); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+
+	user, err := c.db.GetUserByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
-	if err := database.ValidateQuote(updatedQuote); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	updatedQuote.ID = id
-	if err := c.db.ModifyQuote(id, updatedQuote); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(updatedQuote)
+	json.NewEncoder(w).Encode(user)
 }
 
-func (c *serverConfig) DeleteQuoteHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// Extract the quote ID from the URL parameters
+func (c *serverConfig) UpdateUserHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the user ID from the URL parameters
 	idStr := ps.ByName("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid ID format", http.StatusBadRequest)
 		return
 	}
-	if err := c.db.DeleteQuote(id); err != nil {
+	var updatedUser types.User
+	if err := c.readRequestJSON(w, r, &updatedUser); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err := database.ValidateUser(updatedUser); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	updatedUser.UserID = id
+	if err := c.db.UpdateUser(id, updatedUser); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedUser)
+}
+
+func (c *serverConfig) DeleteUserHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the user ID from the URL parameters
+	idStr := ps.ByName("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+	if err := c.db.DeleteUser(id); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
 }
 
-func (c *serverConfig) CreateCommentHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	var comment types.Comment
-	if err := c.readRequestJSON(w, r, &comment); err != nil {
+func (c *serverConfig) CreateNodeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var node types.Node
+	if err := c.readRequestJSON(w, r, &node); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := database.ValidateComment(comment); err != nil {
+	if err := database.ValidateNode(node); err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	if err := c.db.WriteComment(comment); err != nil {
+	if err := c.db.CreateNode(node); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(comment)
+	json.NewEncoder(w).Encode(node)
 }
 
-func (c *serverConfig) GetCommentsHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+func (c *serverConfig) GetNodesHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	// Parse pagination and sorting parameters
 	limit, offset := parsePaginationParams(r)
 	sortBy, sortOrder := parseSortParams(r)
 
-	comments, err := c.db.GetCommentsWithPagination(limit, offset, sortBy, sortOrder)
+	nodes, err := c.db.GetNodesWithPagination(limit, offset, sortBy, sortOrder)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	if len(comments) == 0 {
-		comments = []types.Comment{}
+
+	if len(nodes) == 0 {
+		nodes = []types.Node{}
 	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(comments)
+	json.NewEncoder(w).Encode(nodes)
 }
 
-func (c *serverConfig) UpdateCommentHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// Extract the comment ID from the URL parameters
+func (c *serverConfig) GetNodeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the node ID from the URL parameters
 	idStr := ps.ByName("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid ID format", http.StatusBadRequest)
 		return
 	}
-	var updatedComment types.Comment
-	if err := c.readRequestJSON(w, r, &updatedComment); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+
+	node, err := c.db.GetNodeByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
 		return
 	}
-	if err := database.ValidateComment(updatedComment); err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-	updatedComment.ID = id
-	if err := c.db.ModifyComment(id, updatedComment); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(updatedComment)
+	json.NewEncoder(w).Encode(node)
 }
 
-func (c *serverConfig) DeleteCommentHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// Extract the comment ID from the URL parameters
+func (c *serverConfig) UpdateNodeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the node ID from the URL parameters
 	idStr := ps.ByName("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
 		http.Error(w, "Invalid ID format", http.StatusBadRequest)
 		return
 	}
-	if err := c.db.DeleteComment(id); err != nil {
+	var updatedNode types.Node
+	if err := c.readRequestJSON(w, r, &updatedNode); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if err := database.ValidateNode(updatedNode); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	updatedNode.DeviceID = id
+	if err := c.db.UpdateNode(id, updatedNode); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(updatedNode)
+}
+
+func (c *serverConfig) DeleteNodeHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the node ID from the URL parameters
+	idStr := ps.ByName("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+	if err := c.db.DeleteNode(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (c *serverConfig) CreateNodeDataHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var nodeData types.NodeData
+	if err := c.readRequestJSON(w, r, &nodeData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := database.ValidateNodeData(nodeData); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := c.db.CreateNodeData(nodeData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(nodeData)
+}
+
+func (c *serverConfig) GetNodeDataHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Parse pagination and sorting parameters
+	limit, offset := parsePaginationParams(r)
+	sortBy, sortOrder := parseSortParams(r)
+
+	nodeData, err := c.db.GetNodeDataWithPagination(limit, offset, sortBy, sortOrder)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if len(nodeData) == 0 {
+		nodeData = []types.NodeData{}
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(nodeData)
+}
+
+func (c *serverConfig) GetNodeDataByIDHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the node data ID from the URL parameters
+	idStr := ps.ByName("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+
+	nodeData, err := c.db.GetNodeDataByID(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(nodeData)
+}
+
+func (c *serverConfig) GetNodeDataByDeviceIDHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the device ID from the URL parameters
+	idStr := ps.ByName("deviceId")
+	deviceID, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid device ID format", http.StatusBadRequest)
+		return
+	}
+
+	nodeData, err := c.db.GetNodeDataByDeviceID(deviceID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(nodeData) == 0 {
+		nodeData = []types.NodeData{}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(nodeData)
+}
+
+func (c *serverConfig) GetNodeDataByUserIDHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the user ID from the URL parameters
+	idStr := ps.ByName("userId")
+	userID, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID format", http.StatusBadRequest)
+		return
+	}
+
+	nodeData, err := c.db.GetNodeDataByUserID(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(nodeData) == 0 {
+		nodeData = []types.NodeData{}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(nodeData)
+}
+
+func (c *serverConfig) DeleteNodeDataHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the node data ID from the URL parameters
+	idStr := ps.ByName("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID format", http.StatusBadRequest)
+		return
+	}
+	if err := c.db.DeleteNodeData(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Node Favorites Handlers
+func (c *serverConfig) CreateNodeFavoriteHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var favorite types.NodeFavorite
+	if err := c.readRequestJSON(w, r, &favorite); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := database.ValidateNodeFavorite(favorite); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := c.db.CreateNodeFavorite(favorite); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(favorite)
+}
+
+func (c *serverConfig) GetNodeFavoritesHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	favorites, err := c.db.GetNodeFavorites()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(favorites) == 0 {
+		favorites = []types.NodeFavorite{}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(favorites)
+}
+
+func (c *serverConfig) GetNodeFavoritesByUserIDHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the user ID from the URL parameters
+	idStr := ps.ByName("userId")
+	userID, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID format", http.StatusBadRequest)
+		return
+	}
+
+	favorites, err := c.db.GetNodeFavoritesByUserID(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if len(favorites) == 0 {
+		favorites = []types.NodeFavorite{}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(favorites)
+}
+
+func (c *serverConfig) DeleteNodeFavoriteHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	// Extract the user ID and device ID from the URL parameters
+	userIDStr := ps.ByName("userId")
+	deviceIDStr := ps.ByName("deviceId")
+
+	userID, err := strconv.Atoi(userIDStr)
+	if err != nil {
+		http.Error(w, "Invalid user ID format", http.StatusBadRequest)
+		return
+	}
+
+	deviceID, err := strconv.Atoi(deviceIDStr)
+	if err != nil {
+		http.Error(w, "Invalid device ID format", http.StatusBadRequest)
+		return
+	}
+
+	if err := c.db.DeleteNodeFavorite(userID, deviceID); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
